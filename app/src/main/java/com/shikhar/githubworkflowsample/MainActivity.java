5530a8fe98f6a01359cf71c03febfe24dd09e931
deeps2HOG <shikhar@houseofgod.co>
2021-01-13 19:07:11 +0530

    1st commit

---
+++ app/src/main/java/com/shikhar/githubworkflowsample/MainActivity.java
@@ -0,0 +1,16 @@
+package com.shikhar.githubworkflowsample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.shikhar.githubactionssample.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
